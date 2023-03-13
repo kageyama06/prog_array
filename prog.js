9bd3153309774066b49a53ca3edead3c3a178b8d
Thiago <thiago_r_araujo@estudante.sesisenai.or.br>
2023-03-13 08:22:51 -0300

    incluído o programinha em js

---
+++ prog.js
@@ -0,0 +1,9 @@
+let res=document.getElementById('res')
+let name_city = []
+
+function inserir(){
+    let city = document.getElementById('city').value
+    console.log(city)
+    name_city.push(city)
+    console.log(name_city)
+}
